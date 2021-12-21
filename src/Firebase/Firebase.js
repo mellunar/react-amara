cbf6f539f9f30daf0aabbb7e1b47f3cc11974afe
mellunardev <mellunar100@gmail.com>
2021-12-21 20:27:24 -0300

    update

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,16 +1,11 @@
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
     apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
     databaseURL: process.env.REACT_APP_databaseURL,
     projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId,
   });
 }
@@ -18,6 +13,4 @@ else {
   firebase.app();
 };
 
-export const firebaseAuth = firebase.auth();
 export const db = firebase.firestore();
-export const storage = firebase.storage();
